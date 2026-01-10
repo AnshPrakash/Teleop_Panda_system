18cf269a849d2e8ed7b574a5ea871219f058be74
Ansh Prakash <anshprakash1997@gmail.com>
2026-01-10 15:31:36 +0100

    udpate bash files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,14 @@
+
+# CUDA and locale environment variables
+export CUDA_HOME=/usr/local/cuda-12.6
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH:/home/vignesh/.local/lib
+export LC_NUMERIC=en_US.UTF-8
+
+source /opt/ros/humble/setup.bash
+# source /home/vignesh/poseidon_ws/install/setup.bash
+
+
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro humble -y
 
@@ -9,3 +20,5 @@ colcon build --symlink-install \
     -DPython3_EXECUTABLE=$CONDA_PREFIX/bin/python \
     -DPython3_FIND_STRATEGY=LOCATION \
     -DPython_FIND_STRATEGY=LOCATION
+
+source install/setup.bash
--- setup.sh
+++ setup.sh
@@ -5,18 +5,17 @@ bash ./dependencies/install_ros_humble.sh
 
 
 
-echo '' >> ~/.bashrc
-echo '# CUDA and locale environment variables' >> ~/.bashrc
-echo 'export CUDA_HOME=/usr/local/cuda-12.6' >> ~/.bashrc
-echo 'export PATH=$CUDA_HOME/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH:/home/vignesh/.local/lib' >> ~/.bashrc
-echo 'export LC_NUMERIC=en_US.UTF-8' >> ~/.bashrc
+# CUDA and locale environment variables
+export CUDA_HOME=/usr/local/cuda-12.6
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH:/home/vignesh/.local/lib
+export LC_NUMERIC=en_US.UTF-8
 
+# Install libfranka
 
 # Install franka description
 # bash ./dependencies/install_franka_description.sh
 
-# Install libfranka
 
 
 # Install franka ros2
